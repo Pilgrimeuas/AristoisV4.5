d484e58a8f1c7311d9e5071bdef620e0c9e301dc
xiaojunline <2701804120@qq.com>
2023-07-21 18:10:06 +0800

    improve norender
    inprove norender

--- Horion/Module/Modules/NoRender.h
+++ Horion/Module/Modules/NoRender.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "Module.h"
+#include "../ModuleManager.h"
 
 class NoRender : public IModule {
 public:
+	bool fire = false;
+	bool weather = false;
+	bool nausea = false;
+	bool entities = false;
 	bool blockEntities = false;
 	bool particles = false;
-	bool entities = false;
+	NoRender() : IModule(0x0, Category::VISUAL, "Stop certain animations & unrender objects/entities") {
+		registerBoolSetting("Fire", &fire, fire);
+		registerBoolSetting("Weather", &weather, weather);
+		registerBoolSetting("Nausea", &nausea, nausea);
+		registerBoolSetting("Entites", &entities, entities);
+		registerBoolSetting("Block Entities", &blockEntities, blockEntities);
+		registerBoolSetting("Particles", &particles, particles);
+	}
+	~NoRender() {}
 
-	virtual const char* getRawModuleName();
-	virtual const char* getModuleName();
-	NoRender();
+	virtual const char* getModuleName() override {
+		return "NoRender";
+	}
 };
