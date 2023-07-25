3be381f36c47be7992bd5d35764f7fb4c42f8fca
Hecarium <122374460+Hecarium@users.noreply.github.com>
2023-07-26 02:55:40 +0800

    更新 CrystalUtilsJTWD.h
    翻译注释方便理解

--- Horion/Module/Modules/CrystalUtilsJTWD.h
+++ Horion/Module/Modules/CrystalUtilsJTWD.h
@@ -120,70 +120,69 @@ public:
 	int aSlot;
 	int breakdelay = 0;  // Time to wait (in ticks) until to break a new crystal
 
-	bool retardBreak = false;  // weird super fast break shit
+	bool retardBreak = false;  // 弱智般破坏水晶但是提高速度
 
-	float breakRange = 6.f;  // Range to break endCrystals
+	float breakRange = 6.f;  // 破坏范围
 
-	bool breakAll = true;  // Whether to break ALL the crystals or just the nearest one
+	bool breakAll = true;  // 破坏全部水晶
 
-	float breakWalls = 10;  // How many blocks you are allowed to place through walls
-	float postBWalls = 10;  // Maximum distance to place *after* going through a wall
+	float breakWalls = 10;  // 能穿过多少墙破坏水晶
+	float postBWalls = 10;  // 最多能穿过多少堵墙的距离
 
-	float breakHealth = 5.f;  // What is the minimum health you should have to stop breaking crystals
-	float BmaxSelfDmg = 5.f;  // Maximum damage allowable to player
+	float breakHealth = 5.f;  // 多少伤害停止破坏水晶
+	float BmaxSelfDmg = 5.f;  // 破坏水晶最多对自己造成多少伤害来决定破不破坏这颗水晶
 
-	bool antiWeakness = false;  // detects if you have been hit by weakness and switches to a weapon accordingly
+	bool antiWeakness = false;  // 防止傻逼虚弱狗还没有人修啊
 
-	bool renderBreaking = true;  // Whether to render crystal break attempts
-	float Opacity = 1.f;
-	SettingEnum rotateBreak;  // Rotations (none->obvious
-							  //			  normal->clientside rotate
-							  //			  silent->sends packet to go look at the crystal;
+	bool renderBreaking = true;  // 渲染破坏水晶方框
+	float Opacity = 1.f; // 渲染透明度
+	SettingEnum rotateBreak;  // 破坏转向 (none->obvious
+							  //			  正常转向
+							  //			  发包转向
 
-	bool configHelper = true;
+	bool configHelper = true; //不清楚有什么用
 
 	std::vector<CrystalInfo> CJTWDPlaceArr;
 	vec2_t rotAngle;
 	bool placeArrEmpty = false;
 	bool rotUpNow = false;
 
-	int delay = 0;  // Time to wait (in ticks) until to place a new crystal
+	int delay = 0;  // 延迟多久放水晶
 	// int placetimes = 1;     // Number of times the client should keep retrying to place a crystal
-	int maxProximity = 4;    // What is the maximum distance can a crystal be placed from a person before switching axis
-	float range = 8;         // Range for the enemies to be added to target list
-	float placeRange = 6.f;  // Range to place endCrystals
-	SettingEnum priority;    // Decides how targets are sorted (distance-> lowest to highest
-							 //								   health-> lowest to highest)
-	float thruWallsR = 6;    // How many blocks you are allowed to place through walls
-	float postWallsR = 10;   // Maximum distance to place *after* going through a wall
+	int maxProximity = 4;    // 在切换轴之前，水晶与人之间的最大距离是多少
+	float range = 8;         // 要添加到目标列表的目标的范围人话就是目标范围
+	float placeRange = 6.f;  // 水晶最大的放置范围
+	SettingEnum priority;    // 决定如何对目标进行排序(距离->从最低到最高
+							 //)
+	float thruWallsR = 6;    // 允许您放置多少水晶穿过墙壁
+	float postWallsR = 10;   // 穿墙后放置的最大距离
 
 	/* SOON
 	int lookAhead = 5;		// how many ticks in the future to predict future enemy location (set to 0 to disable predict)
 	*/
 
-	SettingEnum calcDmgType;  // What method is used to calculate damage (java, bedrock)
+	SettingEnum calcDmgType;  // 用什么方法来计算损害（java，基岩版）
 
-	bool safetyFirst = false;  // Whether to prioritize (reduce) self-damage over (maximizing) enemy damage
-	float minHealth = 5.f;     // What is the minimum health you should have to stop placing crystals
-	float maxSelfDmg = 5.f;    // Maximum damage allowable to player
-	float minEnemDmg = 10.f;   // How much damage the crystals do to enemies, ON MINIMUM
+	bool safetyFirst = false;  // 是否优先考虑(减少)自我伤害而不是(最大化)敌人的伤害
+	float minHealth = 5.f;     // 你应该停止放置水晶的最低生命水平是多少
+	float maxSelfDmg = 5.f;    // 允许自己受到的最大伤害
+	float minEnemDmg = 10.f;   // 水晶对敌人造成的伤害，最低限度
+	SettingEnum facePlaceType;  // 控制炸脸放置(none->无脑
+//SMART->仅当敌人处于特定生命状态时才炸脸
+	float fpThresh = 20.f;      // 仅当facePlaceType设置为SMART(如果设置为20)时，client开始炸脸
+	float dmgAtThresh = 3.f;    // 当达到fpThreshold时，水晶需要提供多大的损害
 
-	SettingEnum facePlaceType;  // Controls faceplacing (none->obvious
-								//                       smart->faceplaces only when the enemy is at a certain health)
-	float fpThresh = 20.f;      // Only when facePlaceType is set to smart [if this is set to 20, client always faceplaces]
-	float dmgAtThresh = 3.f;    // When fpThreshold is reached, how much damage do the crystals need to deliver
-
-	bool renderPlacing = true;  // Whether to render place position
-	bool noCheckUpper = false;  // Put true if crystals can fit under blocks(endzone 1.13+ Java)
-	bool attackMulti = false;   // Whether player should try to attack more than 1 enemy
-	SettingEnum switchType;     // Type of Switch (regular->switch to crystal at start
-								//                 switchBack->something like citySwitch in surround
-								//                 APVPSpoof->Uses mob equipment as a spoofer[only works on APVP)
+	bool renderPlacing = true;  // 是否渲染放置位置
+	bool noCheckUpper = false;  // 如果水晶可以放在方块下面（endzone 1.13+ java）人话就是碰撞箱大小确定
+	bool attackMulti = false;   // 自己是否应该尝试攻击1个以上的敌人
+	SettingEnum switchType;     // 切换模式(reg->在启动时切换到水晶
+				 			//                 switchback->类似于物品栏在循环切换
+								//                 APVPSpoof->鬼手
 
 	SettingEnum rotate;  // Rotations (none->obvious
-						 //			  normal->clientside rotate
-						 //			  silent->sends packet to go look at the crystal;
-						 //           pitchUp->so CA works on APVP
+						 //			  normal->正常转向
+						 //			  silent发包转向;
+						 //           pitchUp->抬头转向，但是我们已经bypass了所以这个选项忽略lol
 	void sortCrystals();
 	// void findAFuckingWeapon();
 	/* TODO
@@ -215,7 +214,7 @@ public:
 
 	bool getCrystalSpoof();
 
-	// Inherited via IModule
+	//  via IModule
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
 	virtual void onWorldTick(C_GameMode* gm) override;
